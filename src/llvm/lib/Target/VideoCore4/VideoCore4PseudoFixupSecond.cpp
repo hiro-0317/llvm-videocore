8026ba373f9a75bbf4a378c8edde4b9f9a7bbffc
Toshihiro Konda <kontoshi0317@gamil.com>
2024-08-04 10:52:11 +0900

    update codegen [WIP]

--- src/llvm/lib/Target/VideoCore4/VideoCore4PseudoFixupSecond.cpp
+++ src/llvm/lib/Target/VideoCore4/VideoCore4PseudoFixupSecond.cpp
@@ -212,6 +212,46 @@ VideoCore4PseudoFixupSecond::runOnMachineBasicBlock(MachineBasicBlock &MBB) {
       Changed = true;
     };
 
+    auto OPT_AND_I = [&MBB, MI, TII, I, dl, &Changed](void) {
+      Register reg1 = MI->getOperand(0).getReg();
+      Register reg2 = MI->getOperand(1).getReg();
+      int32_t  imm  = MI->getOperand(2).getImm();
+
+      MBB.erase(MI);
+
+      if (reg1 == reg2 && isInt<16>(imm)) {
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::AND16I_G), reg1)
+	  .addImm(imm);
+      } else {
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::AND32I_G), reg1)
+	  .addReg(reg2)
+	  .addImm(imm);
+      }
+      Changed = true;
+    };
+
+    auto OPT_AND = [&MBB, MI, TII, I, dl, &Changed](void) {
+      Register reg1 = MI->getOperand(0).getReg();
+      Register reg2 = MI->getOperand(1).getReg();
+      Register reg3 = MI->getOperand(2).getReg();
+
+      MBB.erase(MI);
+
+      if (reg1 == reg2) {
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::AND_G), reg1)
+	  .addReg(reg3);
+      } else if (reg1 == reg3) {
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::AND_G), reg1)
+	  .addReg(reg2);
+      } else {
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::MOV_G), reg1)
+	  .addReg(reg3);
+	BuildMI(MBB, I, dl, TII->get(VideoCore4::AND_G), reg1)
+	  .addReg(reg2);
+      }
+      Changed = true;
+    };
+
     switch (Opcode) {
     case VideoCore4::ADD32I_P:
       {
@@ -241,6 +281,16 @@ VideoCore4PseudoFixupSecond::runOnMachineBasicBlock(MachineBasicBlock &MBB) {
     case VideoCore4::SUB_P:
       {
 	OPT_SUB();
+	break;
+      }
+     case VideoCore4::AND32I_P:
+      {
+	OPT_AND_I();
+	break;
+      }
+    case VideoCore4::AND_P:
+      {
+	OPT_AND();
 	break;
       }
     default:
