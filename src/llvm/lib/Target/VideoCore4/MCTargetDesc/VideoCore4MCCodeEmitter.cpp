239c171f99cd7dfa8e8cb8a18f830482d232b662
Toshihiro Konda <kontoshi0317@gamil.com>
2024-08-04 11:03:31 +0900

    update mccode emitter

--- src/llvm/lib/Target/VideoCore4/MCTargetDesc/VideoCore4MCCodeEmitter.cpp
+++ src/llvm/lib/Target/VideoCore4/MCTargetDesc/VideoCore4MCCodeEmitter.cpp
@@ -92,6 +92,13 @@ VideoCore4MCCodeEmitter::encodeInstruction(const MCInst             &MI,
     break;
   }
   case 10: {
+    for (int i = 0; i < Size; ++i) {
+      unsigned Shift = i * 8;
+      APInt    tmp   = Binary;
+      tmp.lshrInPlace(Shift);
+      tmp &= 0xff;
+      CB.push_back((char)(*tmp.getRawData()));
+    }
     break;
   }
   }
